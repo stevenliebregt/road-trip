2512be1e0f7929d9232f86da3013bf48f896272c
Steven Liebregt <s.liebregt@outlook.com>
2018-05-16 08:28:23 +0200

    added some methods in RouteCollectionTest

--- tests/RouteCollectionTest.php
+++ tests/RouteCollectionTest.php
@@ -10,11 +10,114 @@
  */
 
 use PHPUnit\Framework\TestCase;
+use StevenLiebregt\RoadTrip\RouteCollection;
 
 final class RouteCollectionTest extends TestCase
 {
+	/**
+	 * Tests whether a path prefix can be set.
+	 */
+	public function testPathPrefixIsSet(): void
+	{
+		$collection = new RouteCollection();
+		$collection->setPathPrefix('/foo');
+
+		$this->assertSame('/foo', $collection->getPathPrefix());
+	}
+
+	/**
+	 * Tests whether path prefixes get appended successfully.
+	 */
+	public function testPathPrefixIsAppended(): void
+	{
+		$collection = new RouteCollection();
+		$collection->setPathPrefix('/foo');
+
+		$this->assertSame('/foo', $collection->getPathPrefix());
+
+		$collection->setPathPrefix('/bar', true);
+
+		$this->assertSame('/foo/bar', $collection->getPathPrefix());
+	}
+
+	public function testHandlerPrefixIsSet(): void
+	{
+		// TODO:
+	}
+
+	public function testHandlerPrefixIsAppended(): void
+	{
+		// TODO:
+	}
+
+	public function testOptionsAreSet(): void
+	{
+		$collection = new RouteCollection();
+		$collection->setOption('something', 'some-value');
+
+		$this->assertSame('some-value', $collection->getOption('something'));
+		$this->assertSame([
+			'pathPrefix' => null,
+			'handlerPrefix' => null,
+			'something' => 'some-value',
+		], $collection->getOptions());
+
+		$collection->setOptions([
+			'other' => 'thing',
+			'number' => 123.438,
+		]);
+
+		$this->assertSame(123.438, $collection->getOption('number'));
+		$this->assertSame([
+			'pathPrefix' => null,
+			'handlerPrefix' => null,
+			'something' => 'some-value',
+			'other' => 'thing',
+			'number' => 123.438,
+		], $collection->getOptions());
+	}
+
+	/**
+	 * Tests whether the RouteCollection instance has the same options and prefixes after using the
+	 * RouteCollection::clone method.
+	 */
 	public function testHasSameOptionsAfterClone(): void
 	{
-		// TODO: implement
+		// Create collection.
+		$collection = new RouteCollection();
+		$collection->setPathPrefix('/foo');
+		$collection->setOptions([
+			'bar' => 'something',
+			'value' => 4.20,
+		]);
+
+		// Create a clone
+		/** @var RouteCollection $clone */
+		$clone = $this->invokeMethod($collection, 'clone');
+
+		$this->assertEquals($collection, $clone);
+	}
+
+	/**
+	 * Hacky function to allow testing private and protected methods.
+	 *
+	 * @param RouteCollection $object
+	 * @param string $method
+	 * @param array $parameters
+	 *
+	 * @return mixed
+	 * @throws ReflectionException
+	 */
+	private function invokeMethod(RouteCollection &$object, string $method, array $parameters = [])
+	{
+		$reflection = new ReflectionClass(get_class($object));
+		$method = $reflection->getMethod($method);
+		$method->setAccessible(true);
+
+		$result = $method->invokeArgs($object, $parameters);
+
+		$method->setAccessible(false);
+
+		return $result;
 	}
 }
